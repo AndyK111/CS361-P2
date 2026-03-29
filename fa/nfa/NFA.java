7f36c62943e011605c27b733007309fdb511af34
Sam Kleman <spencerkleman@u.boisestate.edu>
2026-03-29 16:18:23 -0600

    implemented eClosure() + fixed isEpsilonTransition()

--- fa/nfa/NFA.java
+++ fa/nfa/NFA.java
@@ -1,5 +1,103 @@
 package fa.nfa;
 
-public class NFA {
+import fa.State;
+import java.util.HashSet;
+import java.util.Set;
+
+public class NFA implements NFAInterface {
+
+    @Override
+    public boolean addState(String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addState'");
+    }
+
+    @Override
+    public boolean setFinal(String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setFinal'");
+    }
+
+    @Override
+    public boolean setStart(String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setStart'");
+    }
+
+    @Override
+    public void addSigma(char symbol) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addSigma'");
+    }
+
+    @Override
+    public boolean accepts(String s) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'accepts'");
+    }
+
+    @Override
+    public Set<Character> getSigma() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getSigma'");
+    }
+
+    @Override
+    public State getState(String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getState'");
+    }
+
+    @Override
+    public boolean isFinal(String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isFinal'");
+    }
+
+    @Override
+    public boolean isStart(String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isStart'");
+    }
+
+    @Override
+    public Set<NFAState> getToState(NFAState from, char onSymb) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getToState'");
+    }
+    /**
+	 * Traverses all epsilon transitions and determine
+	 * what states can be reached from s through e
+	 * @param s
+	 * @return set of states that can be reached from s on epsilon trans.
+	 */
+    @Override
+    public HashSet<NFAState> eClosure(NFAState s) {
+        HashSet<NFAState> closure = s.getAllTransitions();
+        for (NFAState state : closure) {
+            if (s.isEpsilonTransition(state)) {
+                closure.addAll(eClosure(state));
+            }
+        }
+        return closure;
+    }
+
+    @Override
+    public int maxCopies(String s) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'maxCopies'");
+    }
+
+    @Override
+    public boolean addTransition(String fromState, Set<String> toStates, char onSymb) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addTransition'");
+    }
+
+    @Override
+    public boolean isDFA() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isDFA'");
+    }
     
 }
